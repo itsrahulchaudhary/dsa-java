57908ad10cd2a305c2f0b150f223e5d9af4e35e6
itsrahulchaudhary <rahulcy49@gmail.com>
2025-08-26 06:33:51 +0530

    test

--- src/main/java/com/example/dsa/string/S15DuplicateChars.java
+++ src/main/java/com/example/dsa/string/S15DuplicateChars.java
@@ -11,6 +11,8 @@ public class S15DuplicateChars {
             System.out.println("feature/pojo");
         }
         HashMap<Character, Integer> mp = new HashMap<>();
+        System.out.println("I am in develop branch");
+        System.out.println("I am in develop branch Hello sir !!!");
         for(int i=0; i<str.length(); i++){
             if(mp.containsKey(ch[i])){
                 mp.put(ch[i], mp.get(ch[i])+1);
